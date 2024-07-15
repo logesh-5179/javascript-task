f693352d810c46bd4fe2b5a5a511491a7b9cf756
logesh-5179 <logeshmb5179@gmail.com>
2024-07-15 17:37:37 +0530

    Add files via upload

---
+++ javascript/script.js
@@ -0,0 +1,105 @@
+//1
+// let n=100;
+// let temp=[]
+// for (let i=1;i<n;i++) {
+//     if((i%3==0 ) && (i%5==0)){
+//         temp.push("FizzBuzz");
+//     }
+//     else if(i%3==0){
+//         temp.push("Fiz");
+//     }
+//     else if(i%5==0){
+//         temp.push("Buzz");
+
+//     }
+//     else{
+//         temp.push(String(i));
+//     }
+
+// }
+// console.log(temp);
+
+
+//2
+// let str="india";
+// let n=""
+// for(let c=str.length-1;c>=0;c--) {
+//     n+=str.charAt(c);
+// }
+// console.log("Is india a palindrome")
+// console.log(n==str);
+
+
+
+//3
+// let arr=[2,5,6,3,9,1]
+// arr.sort()
+
+// arr.reverse()
+// console.log("lasrgest number in an array is " +arr[0]);
+
+//4
+// let str="america"
+// let a;
+// console.log(str.split(""))
+// let obj={str}
+// for (let i=0;i<obj.length;i++) {
+//      a=Math.max(obj[i].values())
+// }
+// console.log(a)
+//5
+// str="India won the worldcup"
+
+// let s=str.split(" ");
+
+// let x=s.slice(3)
+
+// console.log(x);
+
+
+//6
+// let n=5;
+// let fact=1
+// for (let i=1;i<=n;i++){
+//     fact*=i;
+
+// }
+// console.log("factorial of 5 is " + fact)
+
+
+
+//7
+// function convert(x){
+//     let res=x*32.8;
+//     return res
+// }
+// let x=100;
+// console.log("fahrenheit of 100 is " +convert(x));
+
+
+
+
+
+
+
+
+// 8
+// let arr=[1,3,4,5];
+// let x=[]
+// let y=[]
+// let n=arr.length;
+// for (let i=1;i<n+1;i++){
+   
+//         x.push(i)
+// }
+// for (let i=1;i<x.length;i++){
+//    for(let j=1;j<arr.length;j++){
+//     if(arr[j]!=x[i]){
+//         y.push(arr[j])
+//     }
+// }
+// }
+// console.log(y+" is missing")
+
+
+   
